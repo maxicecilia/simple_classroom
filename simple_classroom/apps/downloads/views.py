f4e38beda4ae4da47946623f940a58165e417718
Maxi Cecilia <maxicecilia@gmail.com>
2015-03-11 23:24:11 -0300

    Fix comments after testing

--- simple_classroom/apps/downloads/views.py
+++ simple_classroom/apps/downloads/views.py
@@ -17,7 +17,7 @@ class DownloadsDictationView(View):
     def get(self, request, *args, **kwargs):
         dictation = Dictation.objects.get_current_or_default(
             site=request.site, default_id=kwargs.get('dictation_id', None))
-        previous_dictations = Dictation.objects.filter(~Q(pk=dictation.pk)).order_by('-year')[:3]
+        previous_dictations = Dictation.objects.filter(~Q(pk=dictation.pk)).order_by('-year')[:2]
         exercises = Assignment.objects.exercises().filter(dictation=dictation)
         midterms = Assignment.objects.published_midterms().filter(dictation=dictation)
         return render_to_response(
---
+++ simple_classroom/static/js/main.js
@@ -0,0 +1,5 @@
+
+/* smooth scrolling for scroll to top */
+$('.scroll-top').click(function(){
+  $('body,html').animate({scrollTop:0},1000);
+})
