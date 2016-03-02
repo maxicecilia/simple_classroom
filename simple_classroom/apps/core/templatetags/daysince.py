3008e6973ed478e59a9039115011f4cf0c008b5d
Gero Nimo <geronimox@gmail.com>
2016-03-02 14:49:35 -0300

    fix days count filter; applied local timezone.

--- simple_classroom/apps/core/templatetags/daysince.py
+++ simple_classroom/apps/core/templatetags/daysince.py
@@ -4,7 +4,7 @@ import datetime
 
 from django import template
 from django.utils.html import avoid_wrapping
-from django.utils.timezone import is_aware, utc
+from django.utils.timezone import is_aware, utc, localtime
 from django.utils.translation import ugettext, ungettext_lazy
 
 register = template.Library()
@@ -16,23 +16,19 @@ def daysince(d, now=None):
     Takes two datetime objects and returns the days between d and now
     as a nicely formatted string, e.g. "10 days".  If d occurs after now,
     then "0 days" is returned.
-    Adapted from
-    https://github.com/django/django/blob/master/django/utils/timesince.py
     """
-    # Convert datetime.date to datetime.datetime for comparison.
-    if not isinstance(d, datetime.datetime):
-        d = datetime.datetime(d.year, d.month, d.day)
-    if now and not isinstance(now, datetime.datetime):
-        now = datetime.datetime(now.year, now.month, now.day)
-
+    #Check if now provided
     if not now:
         now = datetime.datetime.now(utc if is_aware(d) else None)
 
-    delta = (now - d)
-    # ignore microseconds
-    since = delta.days * 24 * 60 * 60 + delta.seconds
-    if since <= 0:
-        # d is in the future compared to now, stop processing.
-        return avoid_wrapping(ugettext('0 minutes'))
-    days_count = since // (60 * 60 * 24)
-    return avoid_wrapping(ungettext_lazy('%d day', '%d days') % days_count)
+    #Apply local timezone to objects and then zero the time part to include
+    #inconcluded days in the count
+    now = localtime(now).replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+    d = localtime(d).replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+
+    #Check for negative delta
+    if d > now:
+        return avoid_wrapping(ugettext('0 days'))
+
+    delta = now - d
+    return avoid_wrapping(ungettext_lazy('%d day', '%d days') % delta.days)
